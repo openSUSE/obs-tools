2727b5f3b6e013f372bff2216980769854bb8e72
Christian Bruckmayer <cbruckmayer@suse.com>
2018-08-08 18:45:39 +0200

    Initial version of pull request package creator bot
    Co-authored-by: Stephan Kulow <coolo@suse.de>

---
+++ pull_request_package/lib/obs_pull_request_package.rb
@@ -0,0 +1,96 @@
+require 'active_model'
+require 'open3'
+
+class ObsPullRequestPackage
+  include ActiveModel::Model
+  attr_accessor :pull_request, :logger, :template_directory
+  
+  def pull_request_number
+    pull_request.number
+  end
+  
+  def commit_sha
+    pull_request.head.sha
+  end
+  
+  def obs_project_name
+    "OBS:Server:Unstable:TestGithub:PR-#{pull_request_number}"
+  end
+  
+  def create
+    create_project
+    create_package
+    copy_files
+  end
+  
+  private
+  
+  def capture2e_with_logs(cmd)
+    logger.info("Execute command '#{cmd}'.")
+    stdout_and_stderr_str, status = Open3.capture2e(cmd)
+    stdout_and_stderr_str.chomp!
+    if status.success?
+      logger.info(stdout_and_stderr_str)
+    else
+      logger.error(stdout_and_stderr_str)
+    end
+    stdout_and_stderr_str
+  end
+  
+  def create_project
+    Tempfile.open("#{pull_request_number}-meta") do |f|
+      f.write(project_meta)
+      f.close
+      capture2e_with_logs("osc meta prj #{obs_project_name} --file #{f.path}")
+    end
+  end
+  
+  def project_meta
+    file = File.read('new_project_template.xml')
+    xml = Nokogiri::XML(file)
+    xml.root['name'] = obs_project_name
+    xml.css('title').first.content = "https://github.com/openSUSE/open-build-service/pull/#{pull_request_number}"
+    xml.to_s
+  end
+  
+  def create_package
+    logger.debug("Creating package #{obs_project_name}")
+    capture2e_with_logs("osc meta pkg #{obs_project_name} obs-server --file new_package_template.xml")
+  end
+  
+  def copy_files
+    Dir.mktmpdir do |dir|
+      capture2e_with_logs("osc co OBS:Server:Unstable/obs-server --output-dir #{dir}/template")
+      capture2e_with_logs("osc co #{obs_project_name}/obs-server --output-dir #{dir}/#{obs_project_name}")
+      copy_package_files(dir)
+      capture2e_with_logs("osc ar #{dir}/#{obs_project_name}")
+      capture2e_with_logs("osc commit #{dir}/#{obs_project_name} -m '#{commit_sha}'")
+    end
+  end
+  
+  def copy_package_files(dir)
+    Dir.entries("#{dir}/template").reject { |name| name.start_with?('.') }.each do |file|
+      path = File.join(dir, 'template', file)
+      target_path = File.join(dir, obs_project_name, file)
+      if file == '_service'
+        copy_service_file(path, target_path)
+      else
+        FileUtils.cp path, target_path
+      end
+    end
+  end
+    
+  def copy_service_file(path, target_path)
+    File.open(target_path, 'w') do |f|
+      f.write(service_file(path))
+    end
+  end
+  
+  def service_file(path)
+    content = File.read(path)
+    xml = Nokogiri::XML(content)
+    node = xml.root.at_xpath(".//param[@name='revision']")
+    node.content = commit_sha
+    xml.to_s
+  end
+end
---
+++ pull_request_package/runner.rb
@@ -0,0 +1,22 @@
+require 'octokit'
+require 'nokogiri'
+require 'tempfile'
+require 'fileutils'
+require 'logger'
+require 'yaml'
+require_relative 'lib/obs_pull_request_package'
+
+config = YAML.load_file('config/config.yml')
+client = Octokit::Client.new(config[:credentials])
+logger = Logger.new(STDOUT)
+
+def line_seperator(pull_request)
+  '=' * 15 + " #{pull_request.title} (#{pull_request.number}) " + '=' * 15
+end
+
+client.pull_requests('openSUSE/open-build-service').each do |pull_request|
+  next if pull_request.updated_at < (Date.today - 7).to_time || pull_request.base.ref != 'master'
+
+  logger.info(line_seperator(pull_request))
+  package = ObsPullRequestPackage.new(pull_request: pull_request, logger: logger).create
+end
