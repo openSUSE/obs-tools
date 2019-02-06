9cd3f0d58690654fadaa59f914b00152e708fa55
Henne Vogelsang <hvogel@opensuse.org>
2019-02-06 17:58:23 +0100

    Remove the depfu and labeling bots
    We do not package individual gems anymore, neither
    do we use commit labels anymore.

--- depfu/bot/runner.rb
+++
@@ -1,7 +0,0 @@
-require 'octokit'
-require './lib/depfu'
-require 'yaml'
-
-config = YAML.load_file('config/config.yml')
-client = Octokit::Client.new(config[:credentials])
-Depfu::PullRequestCommenter.new(client: client).run
